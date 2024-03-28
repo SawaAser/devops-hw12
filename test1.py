f703fee7a386bea5d3e2742cca201aa160000cd3
SawaAser <144454570+SawaAser@users.noreply.github.com>
2024-03-28 13:13:17 +0200

    test

---
+++ test1.py
@@ -0,0 +1,15 @@
+from app import *
+import unittest
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_get_all_product(self):
+        resource = self.app.get('api/products')
+        self.assertEqual(resource.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
